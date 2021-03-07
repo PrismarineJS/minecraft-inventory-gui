8647d70b81a125f620170b1916efbb38e9cd6d29
extremeheat <extreme@protonmail.ch>
2021-03-07 18:21:14 -0500

    clicking+scrolling

--- creative/CreativeWindow.js
+++ creative/CreativeWindow.js
@@ -2,7 +2,7 @@ if (typeof module !== 'undefined') {
   // var { createCanvas } = require('canvas')
   // var fs = require('fs')
   const { getImage } = require('./util')
-  var {windows} = require('./windows')
+  var { windows } = require('./windows')
 } else {
 
 }
@@ -14,14 +14,14 @@ windows.icon = {
 var maxRuns = 40;
 
 class BB {
-  constructor(minX,minY,dx,dy) {
-    this.minX = minX
-    this.minY = minY
-    this.maxX = minX + dx
-    this.maxY = minY + dy
-    this.contains = (x,y) => x > minX && y > minY && x < this.maxX && y < this.maxY
-    this.bbRel = [minX,minY,dx,dy]
-    this.bbAbs = [minX,minY,this.maxX,this.maxY]
+  constructor(minX, minY, dx, dy, xoff=0, yoff=0) {
+    this.minX = minX + xoff
+    this.minY = minY + yoff
+    this.maxX = this.minX + dx
+    this.maxY = this.minY + dy
+    this.contains = (x, y) => x > this.minX && y > this.minY && x < this.maxX && y < this.maxY
+    this.bbRel = [this.minX, this.minY, dx, dy]
+    this.bbAbs = [this.minX, this.minY, this.maxX, this.maxY]
   }
 }
 
@@ -40,41 +40,43 @@ class InvWindow {
     this.lastY = 0
   }
 
-  registerSensitive(regionBB, handler) {
-    this.sensitiveRegions.set(regionBB.bbRel.toString(), [ regionBB, handler ])
+  registerSensitive(regionBB, type, id, handler) {
+    this.sensitiveRegions.set(type + regionBB.bbRel.toString(), [regionBB, id, type, handler])
     // this.sensitiveRegions.set(regionBB, handler)
   }
 
-  getCanvasCoords = function(ctx, screenX, screenY) {
+  getCanvasCoords = function (ctx, screenX, screenY) {
     let matrix = ctx.getTransform();
     var imatrix = matrix.invertSelf();
     let x = screenX * imatrix.a + screenY * imatrix.c + imatrix.e;
     let y = screenX * imatrix.b + screenY * imatrix.d + imatrix.f;
     return [x, y];
-  }  
+  }
 
   tickCursor(evt) {
     var pos = getMousePos(canvas, evt);
-    const [x,y] = this.getCanvasCoords(this.drawCtx, pos.x, pos.y)
+    const [x, y] = this.getCanvasCoords(this.drawCtx, pos.x, pos.y)
 
     clearTimeout(this.int)
     this.int = setTimeout(() => {
       this.lastX = x; this.lastY = y;
       // console.log(this.sensitiveRegions)
       const hits = []
-      for (const [bb,handler] of this.sensitiveRegions.values()) {
-        if (bb.contains(x,y)) {
+      for (const [bb, id, type, handler] of this.sensitiveRegions.values()) {
+        if (bb.contains(x, y) && type.includes('hover')) {
+          // console.log('in',bb)
           hits.push(bb.bbRel)
-          handler()
+          handler(id, 'hover', [x, y])
+        } else {
+          // console.log('not in',x,y,bb)
         }
       }
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
       this.tickRender()
       for (const hit of hits) {
         this.drawBox(hit)
       }
       if (hits.length) {
-        console.warn('Rendered for',hits)
+        console.warn('Rendered for', hits)
       }
     }, 20)
 
@@ -84,7 +86,29 @@ class InvWindow {
 
   }
 
+  tickClick(evt) {
+    var pos = getMousePos(canvas, evt);
+    const [x, y] = this.getCanvasCoords(this.drawCtx, pos.x, pos.y)
+    for (const [bb, id, type, handler] of this.sensitiveRegions.values()) {
+      if (bb.contains(x, y) && type.includes('click')) {
+        console.log('click in',bb)
+        // hits.push(bb.bbRel)
+        handler(id, 'click', [x, y])
+      }
+    }
+  }
+
+  onClick = (id, type, pos) => {
+    
+  }
+
+  onInputBoxInteract = (id, type, pos) => {
+
+  }
+
   tickRender() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    this.index = 0
     this.render(this.renderWindow)
   }
 
@@ -93,6 +117,7 @@ class InvWindow {
       // maxRuns--
       // if (maxRuns<0) throw 1;
       const val = obj[key]
+      if (!val.id) val.id = this.index++
       let _with = val.with
       if (val.using) {
         Object.assign(this, val.using)
@@ -112,8 +137,14 @@ class InvWindow {
         }
       }
       if (val['if']) {
-        if (val['if'] instanceof Function) if (val['if']() != true) continue
-        else if (eval(val['if']) != true) continue
+        const ctx = this;
+        if (val['if'] instanceof Function) {
+          if (val['if']() != true) continue
+          console.log('val',val,val['if'](),'is true')
+        } else if (eval(val['if']) != true) {
+          continue
+          // console.log('val',val,val.if,eval(val['if']),'is true')
+        }
       }
 
       if (val.draw) {
@@ -129,18 +160,54 @@ class InvWindow {
         // console.log('RC',val.x + xoff, val.y + yoff)
         this.render(val.children, val.x + xoff, val.y + yoff)
       } else if (val.type == 'itemgrid') {
+        var i = 0;
+        for (var _x = 0; _x < val.width; _x++) {
+          for (var _y = 0; _y < val.height; _y++) {
+            const bb = [(_x * val.size) + xoff + (_x * val.padding), (_y * val.size) + yoff + (_y * val.padding), val.size, val.size]
+            // this.drawBox()
+            this.drawItem(this[val.containing][i], bb[0], bb[1])
+            this.registerSensitive(new BB(...bb), 'hover+click', val.id, (id, type, pos) => {
 
+            })
+            i++;
+          }
+        }
+      } else if (val.type == 'scrollbar') {
+        // console.log('registered', ...val.bb, xoff, yoff)
+        const bb = new BB(...val.bb, xoff, yoff)
+        // this.drawBox(bb.bbRel)
+        this.registerSensitive(bb, 'hover+click', val.id, (id, type, [ax,ay]) => {
+          const spHeight = val.slice[3]
+          const bbHeight = val.bb[3]
+          // bb.maxY - relPos.y;
+          const rX = bb.maxX-ax
+          const rY = bb.maxY-ay
+          const adjustedY = ay - (spHeight / 2)
+          console.log('sensitive',[ax,ay], rY, rY / bbHeight)
+          this[val.id + 'Increment'] = {x: bb.maxY-ax, y: Math.max(0,Math.min(bbHeight - spHeight, adjustedY - bb.minY)) }
+        })
+        const inc = this[val.id + 'Increment']
+
+        if (inc) {
+          this.drawImage(val, val.x + xoff, val.y + yoff + inc.y, val.slice, val.scale)
+        } else {
+          this.drawImage(val, val.x + xoff, val.y + yoff, val.slice, val.scale)
+        }
       }
 
       if (val.tip) {
         console.assert(val.slice, 'need a slice bb for sensitive region', val)
 
-        const bb = [val.x,val.y,val.slice[2],val.slice[3]]
+        const bb = [val.x, val.y, val.slice[2], val.slice[3]]
         // this.drawBox(bb)
 
-        this.registerSensitive(new BB(...bb), () => {
-          console.log('called',val)
+        this.registerSensitive(new BB(...bb), 'hover', val.id, (id, type, pos) => {
+          console.log('called', val)
         })
+      }
+      if (val.onClick) {
+        const bb = [val.x, val.y, val.slice[2], val.slice[3]]
+        this.registerSensitive(new BB(...bb), 'click', val.id, this.onClick)
       }
 
       // console.log('REC')
@@ -149,6 +216,20 @@ class InvWindow {
         this.render(val.children, xoff, yoff)
       }
     }
+  }
+
+  drawItem(obj, x, y) {
+    const items = ['item/brick',
+    'block/brain_coral',
+    'item/redstone',
+    'block/powered_rail_on',
+    'block/bookshelf',
+    'item/apple',
+    'item/compass_00']
+    const path = items[Math.floor(Math.random() * items.length)];
+    // const path = ''
+    this.drawImage({ path }, x, y)
+    this.drawText({ value: Math.floor(Math.random() * 100), stroke: true, fontStyle: 'bold', px: 8, style: 'white' }, x + 10, y + 16)
   }
 
   drawImage(obj, dx, dy, slice, scale) {
@@ -173,8 +254,14 @@ class InvWindow {
     x ||= 0; y ||= 0;
     x *= this.scale
     y *= this.scale
-    this.drawCtx.font = obj.px + 'px sans-serif'
+    this.drawCtx.font = `${obj.fontStyle || 'normal'} ${obj.fontVariant || 'normal'} ${obj.fontWeight||'normal'} ${obj.px || 11}px sans-serif`
+    this.drawCtx.fillStyle = obj.style || 'black'
     this.drawCtx.fillText(obj.value, x, y)
+    if (obj.stroke) {
+      this.drawCtx.lineWidth = 0.4
+      this.drawCtx.strokeText(obj.value, x, y)
+      this.drawCtx.lineWidth = 1
+    }
     // console.log('drawText', obj, x, y)
   }
 
@@ -189,9 +276,7 @@ class InvWindow {
     // this.drawBox(obj.bb)
     // this.drawCtx.strokeRect(...obj.bb)
 
-    this.registerSensitive(new BB(...obj.bb), () => {
-      console.log('Sensitive region!')
-    })
+    this.registerSensitive(new BB(...obj.bb), 'hover+click', obj.id, this.onInputBoxInteract)
   }
 }
 
@@ -199,12 +284,18 @@ class CreativeWindow extends InvWindow {
   name = "creative"
   activeTab = 5
 
+  hotbar = []
+  bodyItems = []
+
   constructor(ctx) {
     super(ctx)
+    this.tabs = [
+      'undefined', 'tabBuilding', 'tabDecoration', 'tabRedstone', 'tabTransport', 'tabSaved', 'tabSearch', 'tabMisc', 'tabFood', 'tabTools', 'tabCombat', 'tabBrewing', 'tabSurvival'
+    ]
   }
 
   start() {
-    if (JSON.stringify(loadedImageBlobs)=='{}')throw 1;
+    if (JSON.stringify(loadedImageBlobs) == '{}') throw 1;
     this.renderWindow = [windows.Creative]
     super.render(this.renderWindow)
 
@@ -214,12 +305,18 @@ class CreativeWindow extends InvWindow {
     // console.log
 
   }
+
+  onClick = (id, type, pos) => {
+    console.log('Clicked', id, '!')
+    this.activeTab = this.tabs.indexOf(id)
+    this.tickRender()
+  }
 }
 
 const canvas = document.getElementById('demo')
 const ctx = canvas.getContext('2d')
 globalThis.ctx = ctx
-ctx.scale(4,4)
+ctx.scale(4, 4)
 ctx.imageSmoothingEnabled = false;//derp
 const creative = new CreativeWindow(ctx)
 setTimeout(() => creative.start(), 1400)
@@ -229,11 +326,16 @@ canvas.onmousemove = (evt) => {
   creative.tickCursor(evt)
 }
 
+canvas.onclick = (evt) => {
+  // console.log('mouse move', evt)
+  creative.tickClick(evt)
+}
+
 function getMousePos(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
   return {
-      x: (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
-      y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
+    x: (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
+    y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
   };
 }
 
--- creative/windows.js
+++ creative/windows.js
@@ -12,72 +12,81 @@ windows.Creative = {
     'tab_inventory': { path: 'gui/container/creative_inventory/tab_inventory' },
     'tab_items': { path: 'gui/container/creative_inventory/tab_items' },
     'tab_item_search': { path: 'gui/container/creative_inventory/tab_item_search' },
-    'inventoryTabActive': { path: 'gui/container/creative_inventory/tabs', slice: [0, 0, 28, 32] },
+    'inventoryTabActive': { path: 'gui/container/creative_inventory/tabs', slice: [0, 32, 28, 32] },
     'inventoryTab': { path: 'gui/container/creative_inventory/tabs', slice: [0, 0, 28, 32] },
     'inventoryBotTab': { path: 'gui/container/creative_inventory/tabs', slice: [0, 64, 28, 32], y: height + 28 },
-    'inventoryBotActiveTab': { path: 'gui/container/creative_inventory/tabs', slice: [0, 64+32, 20, 20], y: height + 28 },
+    'inventoryBotActiveTab': { path: 'gui/container/creative_inventory/tabs', slice: [0, 64 + 32, 28, 32], y: height + 28 },
     'tabSliderInactive': { path: 'gui/container/creative_inventory/tabs', slice: [232 + 12, 0, 12, 15] },
     'tabSliderActive': { path: 'gui/container/creative_inventory/tabs', slice: [232, 0, 12, 15] },
-    icon: { scale: 1.1, slice: [0,0,16,16] }
+    icon: { scale: 1.1, slice: [0, 0, 16, 16] },
   },
   children: [
     /* Top bar */
-    { type: 'image', with: (ctx) => ctx.activeTab == 1 ? 'inventoryTabActive' : 'inventoryTab', x: 29 * 0, y: 0, tip: 'Building blocks' },
-    { type: 'image', with: (ctx) => ctx.activeTab == 2 ? 'inventoryTabActive' : 'inventoryTab', x: 29 * 1, y: 0, tip: 'Decoration blocks' },
-    { type: 'image', with: (ctx) => ctx.activeTab == 3 ? 'inventoryTabActive' : 'inventoryTab', x: 29 * 2, y: 0, tip: 'Redstone' },
-    { type: 'image', with: (ctx) => ctx.activeTab == 4 ? 'inventoryTabActive' : 'inventoryTab', x: 29 * 3, y: 0, tip: 'Transportation' },
-    { type: 'image', with: (ctx) => ctx.activeTab == 5 ? 'inventoryTabActive' : 'inventoryTab', x: width - 29 * 2, y: 0, tip: 'Saved Inventories' },
-    { type: 'image', with: (ctx) => ctx.activeTab == 6 ? 'inventoryTabActive' : 'inventoryTab', x: width - 29 * 1, y: 0, tip: 'Search' },
+    { type: 'image', id: 'tabBuilding', with: (ctx) => ctx.activeTab == 1 ? 'inventoryTabActive' : 'inventoryTab', x: 29 * 0, y: 0, tip: 'Building blocks', onClick: [] },
+    { type: 'image', id: 'tabDecoration', with: (ctx) => ctx.activeTab == 2 ? 'inventoryTabActive' : 'inventoryTab', x: 29 * 1, y: 0, tip: 'Decoration blocks', onClick: [] },
+    { type: 'image', id: 'tabRedstone', with: (ctx) => ctx.activeTab == 3 ? 'inventoryTabActive' : 'inventoryTab', x: 29 * 2, y: 0, tip: 'Redstone', onClick: [] },
+    { type: 'image', id: 'tabTransport', with: (ctx) => ctx.activeTab == 4 ? 'inventoryTabActive' : 'inventoryTab', x: 29 * 3, y: 0, tip: 'Transportation', onClick: [] },
+    { type: 'image', id: 'tabSaved', with: (ctx) => ctx.activeTab == 5 ? 'inventoryTabActive' : 'inventoryTab', x: width - 29 * 2, y: 0, tip: 'Saved Inventories', onClick: [] },
+    { type: 'image', id: 'tabSearch', with: (ctx) => ctx.activeTab == 6 ? 'inventoryTabActive' : 'inventoryTab', x: width - 29 * 1, y: 0, tip: 'Search', onClick: [] },
     /* Bottom bar */
-    { type: 'image', with: (ctx) => ctx.activeTab == 7 ? 'inventoryBotActiveTab' : 'inventoryBotTab', x: 29 * 0, tip: 'Miscellaneous' },
-    { type: 'image', with: (ctx) => ctx.activeTab == 8 ? 'inventoryBotActiveTab' : 'inventoryBotTab', x: 29 * 1, tip: 'Foodstuffs' },
-    { type: 'image', with: (ctx) => ctx.activeTab == 9 ? 'inventoryBotActiveTab' : 'inventoryBotTab', x: 29 * 2, tip: 'Tools' },
-    { type: 'image', with: (ctx) => ctx.activeTab == 10 ? 'inventoryBotActiveTab' : 'inventoryBotTab', x: 29 * 3, tip: 'Combat' },
-    { type: 'image', with: (ctx) => ctx.activeTab == 11 ? 'inventoryBotActiveTab' : 'inventoryBotTab', x: 29 * 4, tip: 'Brewing' },
-    { type: 'image', with: (ctx) => ctx.activeTab == 12 ? 'inventoryBotActiveTab' : 'inventoryBotTab', x: width - 28, tip: 'Survival' },
-
-    { type: 'image', with: (ctx) => ctx.activeTab == 6 ? 'tab_items' : 'tab_item_search', y: 30 },
+    { type: 'image', id: 'tabMisc', with: (ctx) => ctx.activeTab == 7 ? 'inventoryBotActiveTab' : 'inventoryBotTab', x: 29 * 0, tip: 'Miscellaneous', onClick: [] },
+    { type: 'image', id: 'tabFood', with: (ctx) => ctx.activeTab == 8 ? 'inventoryBotActiveTab' : 'inventoryBotTab', x: 29 * 1, tip: 'Foodstuffs', onClick: [] },
+    { type: 'image', id: 'tabTools', with: (ctx) => ctx.activeTab == 9 ? 'inventoryBotActiveTab' : 'inventoryBotTab', x: 29 * 2, tip: 'Tools', onClick: [] },
+    { type: 'image', id: 'tabCombat', with: (ctx) => ctx.activeTab == 10 ? 'inventoryBotActiveTab' : 'inventoryBotTab', x: 29 * 3, tip: 'Combat', onClick: [] },
+    { type: 'image', id: 'tabBrewing', with: (ctx) => ctx.activeTab == 11 ? 'inventoryBotActiveTab' : 'inventoryBotTab', x: 29 * 4, tip: 'Brewing', onClick: [] },
+    { type: 'image', id: 'tabSurvival', with: (ctx) => ctx.activeTab == 12 ? 'inventoryBotActiveTab' : 'inventoryBotTab', x: width - 28, tip: 'Survival', onClick: [] },
+    /* The actual body */
+    { type: 'image', with: (ctx) => ctx.activeTab == 6 ? 'tab_item_search' : (ctx.activeTab == 12 ? 'tab_inventory' : 'tab_items'), y: 30 },
 
     /* Tab Icons */
     { type: 'image', path: 'item/brick', x: 5, y: 8, with: 'icon' },
-    { type: 'image', path: 'block/brain_coral', x: (29*1) + 5, y: 8, with: 'icon' },//supposed to be bush or something, the cant find icon
-    { type: 'image', path: 'item/redstone', x: (29*2) + 5, y: 8, with: 'icon' },
-    { type: 'image', path: 'block/powered_rail_on', x: (29*3) + 5, y: 8, with: 'icon' },
-    { type: 'image', path: 'block/bookshelf', x: width - (29*2) + 5, y: 8, with: 'icon' },
-    { type: 'image', path: 'item/compass_00', x: width - (29*1) + 5, y: 8, with: 'icon' },
+    { type: 'image', path: 'block/brain_coral', x: (29 * 1) + 5, y: 8, with: 'icon' },//supposed to be bush or something, the cant find icon
+    { type: 'image', path: 'item/redstone', x: (29 * 2) + 5, y: 8, with: 'icon' },
+    { type: 'image', path: 'block/powered_rail_on', x: (29 * 3) + 5, y: 8, with: 'icon' },
+    { type: 'image', path: 'block/bookshelf', x: width - (29 * 2) + 5, y: 8, with: 'icon' },
+    { type: 'image', path: 'item/compass_00', x: width - (29 * 1) + 5, y: 8, with: 'icon' },
 
-    { type: 'image', path: 'item/lava_bucket', x: (29*0) + 5, y: height + 31, with: 'icon' },
-    { type: 'image', path: 'item/apple', x: (29*1) + 5, y: height + 31, with: 'icon' },
-    { type: 'image', path: 'item/iron_axe', x: (29*2) + 5, y: height + 31, with: 'icon' },
-    { type: 'image', path: 'item/golden_sword', x: (29*3) + 5, y: height + 31, with: 'icon' },
-    { type: 'image', path: 'item/glass_bottle', x: (29*4) + 5, y: height + 31, with: 'icon' },//supposed to be water bottle
-    { type: 'image', path: 'item/chest_minecart', x: width - (29*1) + 6, y: height + 31, with: 'icon' },//supposed to be chest
+    { type: 'image', path: 'item/lava_bucket', x: (29 * 0) + 5, y: height + 31, with: 'icon' },
+    { type: 'image', path: 'item/apple', x: (29 * 1) + 5, y: height + 31, with: 'icon' },
+    { type: 'image', path: 'item/iron_axe', x: (29 * 2) + 5, y: height + 31, with: 'icon' },
+    { type: 'image', path: 'item/golden_sword', x: (29 * 3) + 5, y: height + 31, with: 'icon' },
+    { type: 'image', path: 'item/glass_bottle', x: (29 * 4) + 5, y: height + 31, with: 'icon' },//supposed to be water bottle
+    { type: 'image', path: 'item/chest_minecart', x: width - (29 * 1) + 6, y: height + 31, with: 'icon' },//supposed to be chest
     /* Search bar if on search tab */
     {
-      type: 'container', if: 'ctx.activeTab == 5', x: 8, y: 44, children: [
+      type: 'container', if: 'ctx.activeTab == 6', x: 8, y: 44, children: [
         { type: 'text', px: 11, value: L['Search Items'] },
         { type: 'input', bb: [80, 35, 90, 10] },
       ]
     },
     /* Items in the body */
     {
-      type: 'container', if: 'ctx.activeTab != 12', x: 20, y: 60, children: [
-        { type: 'itemgrid', width: 9, height: 9 }
+      type: 'container', if: 'ctx.activeTab != 12', x: 9, y: 47, children: [
+        { type: 'itemgrid', containing: 'bodyItems', width: 9, height: 5, padding: 2, size: 16 }
+      ]
+    },
+    {
+      type: 'container', if: 'ctx.activeTab != 12', x: 9, y: 141, children: [
+        { type: 'itemgrid', containing: 'hotbar', width: 9, height: 1, padding: 2, size: 16 }
       ]
     },
     /* Scrollbar */
     {
       type: 'container', if: 'ctx.activeTab != 12', x: 175, y: 48, children: [
         {
-          if: 'ctx.items.length > (5*9)',
-          draw(ctx, self) {
-            const rowSize = 9
-            const rows = Math.ceil(ctx.items / rowSize)
-            ctx.cursorVertPosInContainer
-            // ctx.drawImage(ctx.tabSliderActive, 0, 0) // TODO: scrolling
-          }
+          type: 'scrollbar', id: 'scrollbar',
+          with: 'tabSliderActive',
+          // if: 'ctx.items.length > (5*9)',
+          bb: [0, 0, 12, 110],
+          // draw(ctx, self) {
+          //   this.scrollbarPosition = 0
+          //   const rowSize = 9
+          //   const rows = Math.ceil(ctx.items / rowSize)
+          //   ctx.cursorVertPosInContainer
+          //   // ctx.drawImage(ctx.tabSliderActive, 0, 0) // TODO: scrolling
+          // }
         },
-        { type: 'image', if: 'ctx.items.length <= (5*9)', x: 0, y: 0, with: 'tabSliderInactive' }
+        // { type: 'image', if: 'ctx.items.length <= (5*9)', x: 0, y: 0, with: 'tabSliderInactive' }
       ]
     }
   ]
