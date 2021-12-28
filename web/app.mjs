6314f555675c3abc7fbaa937723f62d917e7575b
extremeheat <extreme@protonmail.ch>
2021-12-28 11:30:18 -0500

    Lint, add some options to web demo  (#8)
    * lint codebase and add some options to web demo
    
    * add CI
    
    * remove unnececary windows CI
    
    * remove package lock

--- web/app.mjs
+++ web/app.mjs
@@ -3,20 +3,21 @@ import { CanvasEventManager } from '../lib/CanvasManager.mjs'
 import * as InventoryWindows from '../lib/inventories.mjs'
 
 window.canvas = document.getElementById('demo')
-var canvasManager = new CanvasEventManager(canvas)
-canvasManager.setScale(3.5)
+const canvasManager = new CanvasEventManager(window.canvas)
+canvasManager.setScale(3)
+
+const testItems = [
+  'item/brick',
+  'block/brain_coral',
+  'item/redstone',
+  'block/powered_rail_on',
+  'block/bookshelf',
+  'item/apple',
+  'item/compass_00'
+]
 
 const getImageIcon = (item) => {
-  const items = [
-    'item/brick',
-    'block/brain_coral',
-    'item/redstone',
-    'block/powered_rail_on',
-    'block/bookshelf',
-    'item/apple',
-    'item/compass_00'
-  ]
-  return { path: items[item.type], tip: 'item name here' }
+  return { path: testItems[item.type], tip: 'item name here' }
 }
 
 window.inventory = new InventoryWindows.PlayerWin(canvasManager, {
@@ -26,12 +27,59 @@ window.inventory = new InventoryWindows.PlayerWin(canvasManager, {
 setTimeout(() => {
   canvasManager.startRendering()
   console.log('Rendering!')
-}, 2000)
+}, 440)
+
+function randomBetween (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function renderTesting () {
+  const windowMap = window.inventory.getWindowMap()
+
+  for (const key in windowMap) {
+    const size = windowMap[key][1] ? Math.abs(windowMap[key][1] - windowMap[key][0]) + 1 : 1
+    const getRandomItem = () => new window.Item(randomBetween(0, testItems.length - 1), randomBetween(1, 64))
+    const arr = new Array(size).fill(0).map(getRandomItem)
+    window.inventory[key] = arr
+  }
+
+  window.inventory.needsUpdate = true
+}
+
+function clearWindow () {
+  const windowMap = window.inventory.getWindowMap()
+
+  for (const key in windowMap) {
+    window.inventory[key] = []
+  }
+}
+
+window.toggleTesting = () => {
+  globalThis.TESTING = !globalThis.TESTING
+  if (globalThis.TESTING) {
+    renderTesting()
+  } else {
+    clearWindow()
+  }
+}
+
+window.toggleAnimations = () => {
+  globalThis.debuggingInventory = !globalThis.debuggingInventory
+  window.updateWin()
+}
+
+window.setScale = (scale) => {
+  canvasManager.setScale(scale)
+}
 
 window.updateWin = () => {
   canvasManager.reset()
   const selWindow = document.getElementById('active-win').value
-  window.inventory = new InventoryWindows[selWindow](canvasManager, { getImage })
+  window.inventory = new InventoryWindows[selWindow](canvasManager, { getImage, getImageIcon })
   canvasManager.slideInUp(window.inventory)
   window.inventory.needsUpdate = true
+
+  if (globalThis.TESTING) {
+    renderTesting()
+  }
 }
--- web/pwindow.js
+++ web/pwindow.js
@@ -1,15 +1,8 @@
 // const mcData = require('minecraft-data')('1.16')
 
-let PlayerInventory
-
-// , DropDispenseInventory, AnvilInventory,
-//   BeaconInventory, FurnaceInventory, BrewingStandInventory, CraftingTableInventory,
-//   EnchantingTableInventory, GrindstoneInv, HopperInv, LecternInv, LoomInv, VillagerInv,
-//   ShulkerBoxInv, SmokerInv, CartographyInv, StonecutterInv
-
 class PWindowManager {
   mouseDown = false
-  constructor(win, inv) {
+  constructor (win, inv) {
     this.win = win
     this.inv = inv
     this.map = win.getWindowMap()
@@ -17,7 +10,7 @@ class PWindowManager {
 
     win.on('itemEvent', (id, type, pos, data) => {
       // console.log('itemEvent', id, type, pos, data)
-      if (type == 'release') {
+      if (type === 'release') {
         this.onRelease()
       } else {
         const [containing, index] = data
@@ -28,7 +21,7 @@ class PWindowManager {
     })
   }
 
-  setSlot(inventoryIndex, item) {
+  setSlot (inventoryIndex, item) {
     this.inv.slots[inventoryIndex] = item
     // console.log('set', inventoryIndex, item)
     this.renderItems()
@@ -36,7 +29,7 @@ class PWindowManager {
   }
 
   // Called when we've updated the Prismarine-Windows inventory slots, re-render the GUI window
-  renderItems() {
+  renderItems () {
     for (const key in this.map) {
       const [begin, end] = this.map[key]
       this.win[key] = this.inv.slots.slice(begin, end != null ? end + 1 : undefined)
@@ -45,24 +38,24 @@ class PWindowManager {
   }
 
   // Helper for the max amount of items that can be filled in this slot, TODO: fill in with mcData
-  getMaxStackSize(item) {
-    return 64//mcData.items[item.type].stackSize
+  getMaxStackSize (item) {
+    return 64// mcData.items[item.type].stackSize
   }
 
   // Called after the user has held down the mouse and has now released it
-  onRelease() {
+  onRelease () {
     this.mouseDown = false
     this.mouseDownSlots = null
     this.mouseDownFloat = null
-    if (this.win.floatingItem?.count == 0) delete this.win.floatingItem
+    if (this.win.floatingItem?.count === 0) delete this.win.floatingItem
   }
 
-  onLeftClick(inventoryIndex, item) {
+  onLeftClick (inventoryIndex, item) {
     const floating = this.win.floatingItem
     if (floating) {
       console.log('have a floating itm')
       if (item) {
-        if (floating.type == item.type) {
+        if (floating.type === item.type) {
           // add to existing slot
           const free = item.stackSize - item.count
           const consumable = Math.min(floating.count, free)
@@ -71,7 +64,7 @@ class PWindowManager {
           if (floating.count <= 0) delete this.win.floatingItem
           this.win.needsUpdate = true
         } else {
-          //swap
+          // swap
           const old = this.inv.slots[inventoryIndex]
           this.setSlot(inventoryIndex, this.win.floatingItem)
           this.win.floatingItem = old
@@ -89,12 +82,12 @@ class PWindowManager {
     }
   }
 
-  onRightClick(inventoryIndex, slot) {
+  onRightClick (inventoryIndex, slot) {
     const floating = this.win.floatingItem
     if (floating) {
       if (slot) {
         const free = slot.stackSize - slot.count
-        if (slot.type == floating.type && free >= 1) {
+        if (slot.type === floating.type && free >= 1) {
           slot.count++
           floating.count--
         } else {
@@ -112,12 +105,12 @@ class PWindowManager {
       slot.count -= split
       this.win.floatingItem.count = split
     }
-    if (slot?.count == 0) delete this.inv.slots[inventoryIndex]
-    if (floating?.count == 0) delete this.win.floatingItem
+    if (slot?.count === 0) delete this.inv.slots[inventoryIndex]
+    if (floating?.count === 0) delete this.win.floatingItem
   }
 
   // Adds an item to a slot and returns how much was able to be added
-  addToSlot(inventoryIndex, item, existingOnly = true) {
+  addToSlot (inventoryIndex, item, existingOnly = true) {
     const currentItem = this.inv.slots[inventoryIndex]
     if (currentItem) {
       if (currentItem.type !== item.type) return 0
@@ -136,12 +129,12 @@ class PWindowManager {
   }
 
   /**
-   * 
+   *
    * @param {string} slotType - The type of ItemGrid for example the hotbar or armor bar or inventory. The variable used in layout window classes.
    * @param {number} inventoryIndex - The index in the Prismarine-Window slots sent over the network
    * @param {*} item - The item at the position of the `inventoryIndex`
    */
-  onShiftClick(slotType, inventoryIndex, item) {
+  onShiftClick (slotType, inventoryIndex, item) {
     if (!item) return
     // Shift click move item, TODO: handle edge cases:
     // if (type is armor) move to armor slot;
@@ -152,25 +145,25 @@ class PWindowManager {
       const map = this.map[to]
       // First, try to add to add the floating item to all the slots that match the floating item type
       for (let i = map[0]; i < map[1] && item.count; i++) {
-        const added = this.addToSlot(i, item, true)
+        // const added = this.addToSlot(i, item, true)
         // item.count -= added
         if (item.count <= 0) delete this.inv.slots[inventoryIndex]
       }
       // Then if we still have remaining items, add to any empty slot
       for (let i = map[0]; i < map[1] && item.count; i++) {
-        const added = this.addToSlot(i, item, false)
+        // const added = this.addToSlot(i, item, false)
         // item.count -= added
         if (item.count <= 0) delete this.inv.slots[inventoryIndex]
       }
     }
 
-    if (slotType == 'hotbarItems') shift('inventoryItems')
-    if (slotType == 'inventoryItems') shift('hotbarItems')
+    if (slotType === 'hotbarItems') shift('inventoryItems')
+    if (slotType === 'inventoryItems') shift('hotbarItems')
     this.renderItems()
   }
 
   // Called whenever an inventory event occurs
-  onInventoryEvent(type, containing, windowIndex, inventoryIndex, item) {
+  onInventoryEvent (type, containing, windowIndex, inventoryIndex, item) {
     const floating = this.win.floatingItem
     // The user has double clicked, so collect all the items that match this type
     // until the floating (held) stack has reached is its max size
@@ -186,7 +179,7 @@ class PWindowManager {
       for (let i = 0; i < this.inv.slots.length && canPickup; i++) {
         const slot = this.inv.slots[i]
         if (!slot) continue
-        if (floating.type == slot.type && slot.count) {
+        if (floating.type === slot.type && slot.count) {
           canPickup -= slot.count
           floating.count += slot.count
           slot.count = 0
@@ -202,21 +195,21 @@ class PWindowManager {
         }
         this.renderItems()
       }
-    } else if (type == 'click' && this.win._downKeys.has('ShiftLeft')) {
+    } else if (type === 'click' && this.win._downKeys.has('ShiftLeft')) {
       this.onShiftClick(containing, inventoryIndex, item)
-    } else if (type == 'click' || type == 'rightclick') {
+    } else if (type === 'click' || type === 'rightclick') {
       console.log('click with', this.win._downKeys)
-      this[type == 'click' ? 'onLeftClick' : 'onRightClick'](inventoryIndex, item)
+      this[type === 'click' ? 'onLeftClick' : 'onRightClick'](inventoryIndex, item)
       this.mouseDown = type
       this.mouseDownFloat = this.win.floatingItem?.clone() // Backup of held item we start with
       this.mouseDownSlots = new Set([inventoryIndex])
-    } else if (type == 'release') {
+    } else if (type === 'release') {
       // this.mouseDown = false
       // this.mouseDownSlots = null
       // this.mouseDownFloat = null
-    } else if (type == 'hover' && containing == 'inventoryItems' || containing == 'hotbarItems') {
+    } else if (type === 'hover' && (containing === 'inventoryItems' || containing === 'hotbarItems')) {
       if (this.win.floatingItem && this.mouseDownFloat) {
-        if (this.mouseDown == 'click') { // Left clicking
+        if (this.mouseDown === 'click') { // Left clicking
           // multi spread operation
           if (this.mouseDownSlots.has(inventoryIndex) || item) return
           this.mouseDownSlots.add(inventoryIndex)
@@ -234,7 +227,7 @@ class PWindowManager {
           }
           floating.count = accounted
           this.renderItems()
-        } else if (this.mouseDown == 'rightclick') {
+        } else if (this.mouseDown === 'rightclick') {
           // single spread operation
           if (this.mouseDownSlots.has(inventoryIndex)) return
           this.onRightClick(inventoryIndex, item)
@@ -246,16 +239,19 @@ class PWindowManager {
 }
 
 class Item {
-  constructor(type, count) {
+  constructor (type, count) {
     this.type = type
     this.count = count
     this.displayName = type
     this.stackSize = 64
   }
-  clone() {
+
+  clone () {
     return new Item(this.type, this.count)
   }
 }
+
+window.Item = Item
 
 class PWindow {
   slots = [
@@ -265,18 +261,18 @@ class PWindow {
     new Item(5, 15)
   ]
 
-  firstEmptySlotRange(start, end) {
+  firstEmptySlotRange (start, end) {
     for (let i = start; i < end; ++i) {
       if (!this.slots[i]) return i
     }
     return null
   }
 
-  firstEmptyHotbarSlot() {
+  firstEmptyHotbarSlot () {
     return this.firstEmptySlotRange(this.hotbarStart, this.inventoryEnd)
   }
 
-  firstEmptyContainerSlot() {
+  firstEmptyContainerSlot () {
     return this.firstEmptySlotRange(0, this.inventoryStart)
   }
 }
@@ -285,7 +281,7 @@ const pwindow = new PWindow()
 
 setTimeout(() => {
   window.manager = new PWindowManager(window.inventory, pwindow)
-}, 2200)
+}, 500)
 
 // function createWindowView(pwindow) {
 //   let windows = {}
